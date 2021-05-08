0e07eabe980ff6cf03ab6812224bbcd28654322a
Taeho Kim <taeho@taeho.io>
2021-05-08 18:58:27 +0900

    Add IAM bindings

--- global/artifactregistry.go
+++ global/artifactregistry.go
@@ -3,15 +3,32 @@ package main
 import (
 	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/artifactregistry"
 	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/organizations"
+	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/projects"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/taehoio/iac"
 )
 
 func newDockerRegistry(ctx *pulumi.Context, project *organizations.Project) (*artifactregistry.Repository, error) {
-	return artifactregistry.NewRepository(ctx, "docker-registry", &artifactregistry.RepositoryArgs{
+	repo, err := artifactregistry.NewRepository(ctx, "docker-registry", &artifactregistry.RepositoryArgs{
 		Project:      project.ProjectId,
 		Location:     pulumi.String(iac.TokyoLocation),
 		Format:       pulumi.String("DOCKER"),
 		RepositoryId: pulumi.String("docker-registry"),
 	}, pulumi.Protect(false))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = projects.NewIAMBinding(ctx, "taehoio-staging-project-docker-registry-read", &projects.IAMBindingArgs{
+		Project: project.ProjectId,
+		Members: pulumi.StringArray{
+			pulumi.String("serviceAccount:service-879516865171@serverless-robot-prod.iam.gserviceaccount.com"),
+		},
+		Role: pulumi.String("roles/artifactregistry.reader"),
+	}, pulumi.Protect(false))
+	if err != nil {
+		return nil, err
+	}
+
+	return repo, nil
 }
--- global/serviceaccount.go
+++ global/serviceaccount.go
@@ -2,22 +2,55 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/organizations"
+	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/projects"
 	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/serviceaccount"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func newPulumiCICDServiceAccount(ctx *pulumi.Context, project *organizations.Project) (*serviceaccount.Account, error) {
-	return serviceaccount.NewAccount(ctx, "pulumi-cicd", &serviceaccount.AccountArgs{
+	sa, err := serviceaccount.NewAccount(ctx, "pulumi-cicd", &serviceaccount.AccountArgs{
 		Project:     project.ProjectId,
 		AccountId:   pulumi.String("pulumi-cicd"),
 		DisplayName: pulumi.String("pulumi-cicd"),
 	}, pulumi.Protect(false))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = projects.NewIAMBinding(ctx, "pulumi-cicd-owner", &projects.IAMBindingArgs{
+		Project: project.ProjectId,
+		Members: pulumi.StringArray{
+			pulumi.String("serviceAccount:pulumi-cicd@taehoio-global.iam.gserviceaccount.com"),
+		},
+		Role: pulumi.String("roles/owner"),
+	}, pulumi.Protect(false))
+	if err != nil {
+		return nil, err
+	}
+
+	return sa, nil
 }
 
 func newDockerRegistryServiceAccount(ctx *pulumi.Context, project *organizations.Project) (*serviceaccount.Account, error) {
-	return serviceaccount.NewAccount(ctx, "docker-registry", &serviceaccount.AccountArgs{
+	sa, err := serviceaccount.NewAccount(ctx, "docker-registry", &serviceaccount.AccountArgs{
 		Project:     project.ProjectId,
 		AccountId:   pulumi.String("docker-registry"),
 		DisplayName: pulumi.String("docker-registry"),
 	}, pulumi.Protect(false))
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = projects.NewIAMBinding(ctx, "docker-registry-repo-admin", &projects.IAMBindingArgs{
+		Project: project.ProjectId,
+		Members: pulumi.StringArray{
+			pulumi.String("serviceAccount:docker-registry@taehoio-global.iam.gserviceaccount.com"),
+		},
+		Role: pulumi.String("roles/artifactregistry.repoAdmin"),
+	}, pulumi.Protect(false))
+	if err != nil {
+		return nil, err
+	}
+
+	return sa, nil
 }
